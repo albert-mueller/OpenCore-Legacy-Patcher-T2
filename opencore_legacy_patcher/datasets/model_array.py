480c57008414d5d59b6dd40b74539e902339c898
Albert Müller <p8bpg9zrw7@privaterelay.appleid.com>
2026-04-27 22:08:49 +0300

    Blocking Build OpenCore for 2018 and 2019 MacBook Airs
    From the next release onwards, the MacBookAir8,1 and 8,2 users will see that the Build OpenCore will be blacked out. This is due to not only the T2 chip, but the limitations of these exact Y series processors and the UEFI combined that simply can't handle OpenCore properly at least for now.

--- opencore_legacy_patcher/datasets/model_array.py
+++ opencore_legacy_patcher/datasets/model_array.py
@@ -24,8 +24,8 @@ SupportedSMBIOS = [
     "MacBookAir6,2",
     "MacBookAir7,1",
     "MacBookAir7,2",
-    "MacBookAir8,1",
-    "MacBookAir8,2",
+    # "MacBookAir8,1",
+    # "MacBookAir8,2",
     "MacBookAir9,1",
     # MacBook Pro
     "MacBookPro4,1",
