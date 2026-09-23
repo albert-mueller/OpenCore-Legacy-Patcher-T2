d00935b24d1afb5afacd7e0ab0d2694e04d03ce4
Albert Müller <p8bpg9zrw7@privaterelay.appleid.com>
2026-09-23 21:49:21 +0300

    Update patcher version to 4.0.0.190004.2

--- opencore_legacy_patcher/constants.py
+++ opencore_legacy_patcher/constants.py
@@ -16,7 +16,7 @@ class Constants:
     def __init__(self) -> None:
         # Patcher Versioning
         # Wenn eine Version mit s endet, es heißt, dass sie noch nicht fertig ist.
-        self.patcher_version:                 str = "4.0.0.190004.1"
+        self.patcher_version:                 str = "4.0.0.190004.2"
         self.patcher_version_label=self.patcher_version
         self.patcher_support_pkg_version:     str = "2.0.3"  # PatcherSupportPkg
         self.copyright_date:                  str = "Copyright © 2020-2026 Dortania · T2 fork © 2026 Albert Müller"
